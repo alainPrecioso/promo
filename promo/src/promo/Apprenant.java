763f8d500e37e052835c48e6c22f0e122f030966
Alain Precioso <alainprecioso@gmail.com>
2022-06-20 11:15:54 +0200

    création classe apprenants

---
+++ promo/src/promo/Apprenant.java
@@ -0,0 +1,71 @@
+package promo;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public abstract class Apprenant {
+	
+	protected String nom;
+	protected String prenom;
+	protected LocalDate dateInscri;
+	protected ArrayList<String> contacts;
+	protected String nomEntreprise;
+	protected int retards; //minutes
+	protected int asbsences; //jours
+	protected String promo;
+	
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public LocalDate getDateInscri() {
+		return dateInscri;
+	}
+	public void setDateInscri(LocalDate dateInscri) {
+		this.dateInscri = dateInscri;
+	}
+	public ArrayList<String> getContacts() {
+		return contacts;
+	}
+	public void setContacts(ArrayList<String> contacts) {
+		this.contacts = contacts;
+	}
+	public String getNomEntreprise() {
+		return nomEntreprise;
+	}
+	public void setNomEntreprise(String nomEntreprise) {
+		this.nomEntreprise = nomEntreprise;
+	}
+	public int getRetards() {
+		return retards;
+	}
+	public void setRetards(int retards) {
+		this.retards = retards;
+	}
+	public int getAsbsences() {
+		return asbsences;
+	}
+	public void setAsbsences(int asbsences) {
+		this.asbsences = asbsences;
+	}
+	public String getPromo() {
+		return promo;
+	}
+	public void setPromo(String promo) {
+		this.promo = promo;
+	}
+	
+	
+	
+	
+	
+
+}
