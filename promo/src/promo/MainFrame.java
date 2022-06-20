785aa81fdef1ce15015cc21cf3d502eeeed5b893
Alain Precioso <alainprecioso@gmail.com>
2022-06-20 18:03:20 +0200

    création mainframe

---
+++ promo/src/promo/MainFrame.java
@@ -0,0 +1,57 @@
+package promo;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MainFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1149, 527);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panelN = new JPanel();
+		contentPane.add(panelN, BorderLayout.NORTH);
+		
+		JPanel panelS = new JPanel();
+		contentPane.add(panelS, BorderLayout.SOUTH);
+		
+		JPanel panelW = new JPanel();
+		contentPane.add(panelW, BorderLayout.WEST);
+		
+		JPanel panelC = new JPanel();
+		contentPane.add(panelC, BorderLayout.CENTER);
+		
+		JPanel panelE = new JPanel();
+		contentPane.add(panelE, BorderLayout.EAST);
+	}
+
+}
