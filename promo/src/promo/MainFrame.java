9d963ed1e80a0f3bdb153f1ba371e8a36f30f4c8
Alain Precioso <alainprecioso@gmail.com>
2022-06-20 21:02:48 +0200

    début de fenêtre

--- promo/src/promo/MainFrame.java
+++ promo/src/promo/MainFrame.java
@@ -2,14 +2,22 @@ package promo;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JList;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MainFrame extends JFrame {
 
 	private JPanel contentPane;
+	JComboBox comboBox;
+	ArrayList<Promo> promos = new ArrayList<Promo>();
 
 	/**
 	 * Launch the application.
@@ -40,6 +48,27 @@ public class MainFrame extends JFrame {
 		
 		JPanel panelN = new JPanel();
 		contentPane.add(panelN, BorderLayout.NORTH);
+		panelN.setLayout(new BorderLayout(0, 0));
+		
+		JList generalJList = new JList();
+		panelN.add(generalJList, BorderLayout.SOUTH);
+		
+		
+		
+		promos.add(genererPromo());
+		promos.add(genererPromo2());
+		comboBox = new JComboBox(promos.toArray());
+		panelN.add(comboBox, BorderLayout.NORTH);
+		
+		JButton afficherPromo = new JButton("Afficher Promo");
+		afficherPromo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Promo afficherPromo = (Promo) comboBox.getSelectedItem();
+				generalJList.setListData(afficherPromo.getEleve().toArray());
+				
+			}
+		});
+		panelN.add(afficherPromo, BorderLayout.EAST);
 		
 		JPanel panelS = new JPanel();
 		contentPane.add(panelS, BorderLayout.SOUTH);
@@ -52,6 +81,41 @@ public class MainFrame extends JFrame {
 		
 		JPanel panelE = new JPanel();
 		contentPane.add(panelE, BorderLayout.EAST);
+	}
+	
+	public Promo genererPromo() {
+		ArrayList<String> contact = new ArrayList<String>();
+		ArrayList<Apprenant> list = new ArrayList<Apprenant>();
+		list.add(new Alternant("john", "john", 2022, 04, 01, contact, "optimus", 0, 0, "java", "2500"));
+		list.add(new Alternant("jones", "boby", 2022, 03, 01, contact, "domédia", 0, 0, "java", "1800"));
+		list.add(new Alternant("bertrand", "rene", 2022, 05, 01, contact, "lava", 0, 0, "java", "1500"));
+		list.add(new Alternant("michel", "jacky", 2022, 04, 01, contact, "biotech", 0, 0, "java", "1650"));
+		list.add(new Alternant("petit", "dilan", 2022, 03, 01, contact, "eureka", 0, 0, "java", "1977"));
+
+		list.add(new Stagiaire("kurilenko", "olga", 2022, 04, 01, contact, "optimus",0,0, "java","are", 1010));
+		list.add(new Stagiaire("boutin", "louis", 2022, 07, 01, contact, "optimus", 0, 0, "java","aref", 1101));
+		list.add(new Stagiaire("ceasar", "harry", 2022, 05, 01, contact, "optimus", 0, 0, "java","are", 1200));
+		list.add(new Stagiaire("stark", "aria", 2022, 04, 01, contact, "optimus", 0, 0, "java", "are", 1300));
+		list.add(new Stagiaire("bella", "erika", 2022, 04, 01, contact, "optimus", 0, 0, "java", "aref", 1360));
+		Promo promo = new Promo(list, "Java", 2022, 05, 02, 120);
+		return promo;
+	}
+	public Promo genererPromo2() {
+		ArrayList<String> contact = new ArrayList<String>();
+		ArrayList<Apprenant> list = new ArrayList<Apprenant>();
+		list.add(new Alternant("john", "john", 2022, 04, 01, contact, "optimus", 0, 0, "java", "2500"));
+		list.add(new Alternant("jones", "boby", 2022, 03, 01, contact, "domédia", 0, 0, "java", "1800"));
+		list.add(new Alternant("bertrand", "rene", 2022, 05, 01, contact, "lava", 0, 0, "java", "1500"));
+		list.add(new Alternant("michel", "jacky", 2022, 04, 01, contact, "biotech", 0, 0, "java", "1650"));
+		list.add(new Alternant("petit", "dilan", 2022, 03, 01, contact, "eureka", 0, 0, "java", "1977"));
+
+		list.add(new Stagiaire("kurilenko", "olga", 2022, 04, 01, contact, "optimus",0,0, "java","are", 1010));
+		list.add(new Stagiaire("boutin", "louis", 2022, 07, 01, contact, "optimus", 0, 0, "java","aref", 1101));
+		list.add(new Stagiaire("ceasar", "harry", 2022, 05, 01, contact, "optimus", 0, 0, "java","are", 1200));
+		list.add(new Stagiaire("stark", "aria", 2022, 04, 01, contact, "optimus", 0, 0, "java", "are", 1300));
+		list.add(new Stagiaire("bella", "erika", 2022, 04, 01, contact, "optimus", 0, 0, "java", "aref", 1360));
+		Promo promo = new Promo(list, "PHP", 2022, 05, 02, 120);
+		return promo;
 	}
 
 }
--- promo/src/promo/Promo.java
+++ promo/src/promo/Promo.java
@@ -13,11 +13,10 @@ public class Promo {
 	
 	
 	
-	public Promo(ArrayList<Apprenant> eleve, String nomPromo, LocalDate dateDebut, Integer duree) {
-		super();
+	public Promo(ArrayList<Apprenant> eleve, String nomPromo, Integer annee, Integer mois, Integer jour, Integer duree) {
 		this.eleve = eleve;
 		this.nomPromo = nomPromo;
-		this.dateDebut = dateDebut;
+		this.dateDebut = LocalDate.of(annee, mois, jour);
 		this.duree = duree;
 	}
 	
@@ -36,10 +35,41 @@ public class Promo {
 		for (Apprenant apprenant : eleve) {
 			if (apprenant.getAbsences() > (double)( duree * 0.1 )) {
 				apprenant.setAlertAbscences(true);
-				
 			}
 		}
 	}
+
+	
+	public ArrayList<Apprenant> getEleve() {
+		return eleve;
+	}
+
+	public void setEleve(ArrayList<Apprenant> eleve) {
+		this.eleve = eleve;
+	}
+
+	public String getNomPromo() {
+		return nomPromo;
+	}
+
+	public void setNomPromo(String nomPromo) {
+		this.nomPromo = nomPromo;
+	}
+
+	public LocalDate getDateDebut() {
+		return dateDebut;
+	}
+
+	public void setDateDebut(LocalDate dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+
+	@Override
+	public String toString() {
+		return "Promo=" + nomPromo + ", dateDebut=" + dateDebut + ", duree=" + duree;
+	}
+	
+	
 	
 
 }
