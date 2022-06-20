7acf41b3d450c7f76040dee960b49088c4ca053b
chrisd <chrisd@localhost>
2022-06-20 14:51:09 +0200

    n

--- promo/src/promo/Main.java
+++ promo/src/promo/Main.java
@@ -1,10 +1,16 @@
 package promo;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
+ArrayList<String> contact = new ArrayList<String>();
+ArrayList<Apprenant> list = new ArrayList<Apprenant>();
+list.add(new Alternant("john", "john", 2022, 04, 01, contact, "optimus", 0, 0, "java", "2500"));
+	
+System.out.println(list);
 
 	}
 
