fb8a1ed7c976ed26af7ff1932de8246b04ed72fc
r.cilia <r.cilia@ICS-JAVA-2.pedago.ccinca.fr>
2022-06-22 08:48:41 +0200

    commit

---
+++ promo/src/promo/Panneau.java
@@ -0,0 +1,16 @@
+package promo;
+
+import javax.swing.JFrame;
+
+public class Panneau extends JFrame {
+
+	public Panneau() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
