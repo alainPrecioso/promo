edfa44edcfb534b9d5d5d2abdea77c0d92bfe71b
Alain Precioso <alainprecioso@gmail.com>
2022-06-24 19:30:20 +0200

    fin merge main

--- promo/src/promo/Alternant.java
+++ promo/src/promo/Alternant.java
@@ -1,28 +1,53 @@
 package promo;
 
-public class Alternant {
+import java.util.ArrayList;
+
+public class Alternant extends Apprenant{
 	protected String salaire;
 
 	public Alternant() {
 		
 	}
 
-	public Alternant(String salaire) {
-		super();
-		this.salaire = salaire;
+	public Alternant(String nom, String prenom, Integer annee, Integer mois, Integer jour, ArrayList<String> contacts,
+			String promo, String nomEntreprise, String salaire ) {
+		super(nom, prenom, annee, mois, jour, contacts, promo, nomEntreprise);
+	this.salaire = salaire;
+	
 	}
-
-	protected String getSalaire() {
+	
+	public String getSalaire() {
 		return salaire;
 	}
 
-	protected void setSalaire(String salaire) {
+	public void setSalaire(String salaire) {
 		this.salaire = salaire;
 	}
 
 	@Override
 	public String toString() {
-		return "Alternant [Salaire=" + salaire + "]";
+		String str = prenom+ " " +nom+ " || Date inscription: " + dateInscri;
+		
+		if (alertAbsences == true || alertRetards == true) {
+			str += " ALERTE";
+		}
+		return str;
 	}
+	
+	@Override
+	public String toStringComplet() {
+		String str = prenom+", "+nom+", "+dateInscri+", "
+				 +promo+", "+salaire+"€, salarié à "+nomEntreprise+", "
+				 +retards+" minutes, "+absences + " jours";
+		
+		if (alertAbsences == true || alertRetards == true) {
+			str += " ALERTE";
+		}
+		return str;
+	}
+	
 
+
+
+	
 }
