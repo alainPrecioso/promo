b67c2ec727888049d995573cf8adf20c3fdc904f
Alain Precioso <alainprecioso@gmail.com>
2022-06-24 10:28:35 +0200

    plus de commentaires

--- promo/src/promo/MainFrame.java
+++ promo/src/promo/MainFrame.java
@@ -38,7 +38,7 @@ public class MainFrame extends JFrame {
 	JPanel contentPane; //panneau principal
 	JComboBox comboBox; //menu déroulant qui contiendra les promos
 	DefaultComboBoxModel promoBox; //model pour le menu déroulant
-	//chargement de la database
+	//chargement et tri de la database
 	ArrayList<Promo> promos = PSort.sort((ArrayList<Promo>) Ser.load("promos.xml"));
 	JList detailedList; //infos détaillés d'un apprenant
 	JTextField textField; //field pour entrer les retards ou absences
@@ -62,47 +62,47 @@ public class MainFrame extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+	//constructeur de MainFrame
 	public MainFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1149, 527);
-		contentPane = new JPanel();
+		contentPane = new JPanel(); //panel principal
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 
+		//panel Nord, contient le menu déroulant, la liste des élèves, les boutons d'ajouts et d'alerte
 		JPanel panelN = new JPanel();
 		contentPane.add(panelN, BorderLayout.NORTH);
 		panelN.setLayout(new BorderLayout(0, 0));
 
-		DefaultListModel model = new DefaultListModel();
+		DefaultListModel model = new DefaultListModel();// model pour la JList
 		JList generalJList = new JList(model);
+		//MouseListener qui lance printApprenant() quand on click dans la liste
 		generalJList.addMouseListener(new MouseAdapter(){
 			public void mouseClicked(MouseEvent evt) {
-				printApprenant(generalJList);
+				printApprenant(generalJList); //method qui affiche les infos détaillées d'un apprenant 
 			}
 		});
 		
 		panelN.add(generalJList, BorderLayout.SOUTH);
-
 		promoBox = new DefaultComboBoxModel(promos.toArray());
 		comboBox = new JComboBox(promoBox);
 		panelN.add(comboBox, BorderLayout.NORTH);
-		comboBox.setSelectedIndex(-1);
-		
+		comboBox.setSelectedIndex(-1); //permet d'afficher le menu déroulant vide au démarrage
+		//ActionListener qui lance printPromo() quand on choisit un element dans le menu déroulant
 		comboBox.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				printPromo(model, panelN);
+				printPromo(model, panelN); //method qui affiche les élèves de la promo séléctionnée
 
 			}
 		});
 		
+		//panel pour ranger le boutton alerte
 		JPanel panel = new JPanel();
 		panelN.add(panel, BorderLayout.EAST);
 		
+		//boutton pour afficher tous les élèves qui sont flagués pour retards ou absences
 		JButton alertesButton = new JButton("Alertes");
 		alertesButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -114,10 +114,12 @@ public class MainFrame extends JFrame {
 		JPanel panelNewPromo = new JPanel();
 		panelN.add(panelNewPromo, BorderLayout.WEST);
 		
+		//"JButton newPromo" what else is there to say?
 		JButton newPromo = new JButton("Création promo");
 		panelNewPromo.add(newPromo);
 		newPromo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				//création d'un nouvel objet NFenetre
 				EventQueue.invokeLater(new Runnable() {
 					public void run() {
 						try {
@@ -128,11 +130,13 @@ public class MainFrame extends JFrame {
 						}
 					}
 				});
+				//fermeture de la MainFrame
 				((MainFrame) ((Component)e.getSource()).getParent().getParent().getParent().getParent().getParent().getParent()).dispose();
 				
 			}
 		});
 		
+		// "JButton newApprenant" see above
 		JButton newApprenant = new JButton("Création Apprenant");
 		panelNewPromo.add(newApprenant);
 		newApprenant.addActionListener(new ActionListener() {
@@ -152,33 +156,30 @@ public class MainFrame extends JFrame {
 		});
 		
 		panelN.add(timeLabel, BorderLayout.CENTER);
-		
-		
-		
-		
-		
-		
-		
 
+		//panneau au Sud du panneau principal
 		JPanel panelS = new JPanel();
 		contentPane.add(panelS, BorderLayout.SOUTH);
 		panelS.setLayout(new BorderLayout(0, 0));
 		
-		detailedList = new JList();
+		detailedList = new JList(); //liste qui contendra les infos détaillés d'un apprenant
 		panelS.add(detailedList, BorderLayout.SOUTH);
 		
-		JPanel panelSC = new JPanel();
+		//panneau qui contiendra la zone de texte et les bouttons pour les retards/absences
+		JPanel panelSC = new JPanel(); 
 		panelS.add(panelSC, BorderLayout.CENTER);
 		
-		textField = new JTextField();
+		textField = new JTextField(); //zone de texte pour ajout de retard/absence
 		textField.setVisible(false);
 		panelSC.add(textField);
 		textField.setColumns(10);
 		
+		//self explanatory
 		retardButton = new JButton("Ajout Retard");
 		retardButton.setVisible(false);
 		retardButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				//ajoute le retard entré dans la zone de texte à l'élève puis sauvegarde
 				promos.get(comboBox.getSelectedIndex()).getEleve().get(spot).addRetards(Integer.valueOf(textField.getText()));
 				printApprenant(generalJList);
 				Ser.save("promos", promos);
@@ -186,10 +187,12 @@ public class MainFrame extends JFrame {
 		});
 		panelSC.add(retardButton);
 		
+		//see above
 		absenceButton = new JButton("Ajout Absence");
 		absenceButton.setVisible(false);
 		absenceButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				//ajoute l'absence entrée dans la zone de texte à l'élève puis sauvegarde
 				promos.get(comboBox.getSelectedIndex()).getEleve().get(spot).addAbsences(Integer.valueOf(textField.getText()));
 				printApprenant(generalJList);
 				Ser.save("promos", promos);
@@ -200,31 +203,33 @@ public class MainFrame extends JFrame {
 		
 	}
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+	//method qui affiche une liste de tous les apprenants flagués pour retard/absence
 	public void printAlertes (DefaultListModel model) {
-		model.clear();
-		timeLabel.setText("");
-		for (Promo promo : promos) {
-			for (Apprenant apprenant : promo.getEleve()) {
+		model.clear(); //vide la liste
+		timeLabel.setText(""); //vide l'affichage temps passé
+		for (Promo promo : promos) { //pour chaque Promo dans promos
+			for (Apprenant apprenant : promo.getEleve()) {// pour chaque Apprenant dans la promo
+				//si l'apprenant est flagué
 				if (apprenant.isAlertAbsences() == true || apprenant.isAlertRetards() == true) {
-					model.addElement(apprenant);
+					model.addElement(apprenant); //ajouter l'Apprenant au modèle
 				}
 			}
 		}
 		
 	}
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+	
+	//method qui récupère la promo séléctionnée dans le menu déroulant pour afficher ses élèves
 	public void printPromo(DefaultListModel model, JPanel panelN) {
 		affichagePromo = (Promo) comboBox.getSelectedItem();
 		model.clear();
 		model.addAll(affichagePromo.getEleve());
-		//generalJList.setListData(afficherPromo.getEleve().toArray());
+		//string pour afficher les jours passés et restants dans la formation
 		String jr = nbJoursSemaine(affichagePromo.getDateDebut().atStartOfDay(), LocalDate.now().atStartOfDay());
 		String jr2 = String.valueOf(affichagePromo.getDuree() - Integer.valueOf(jr));
 		timeLabel.setText(jr + " jours passés, " + jr2 + " jours restants");
 	}
 	
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+	
 	public void printApprenant(JList generalJList) {
 		textField.setVisible(true);
 		retardButton.setVisible(true);
@@ -241,7 +246,6 @@ public class MainFrame extends JFrame {
 			selectedApprenant.add(app.toStringComplet());
 			selectedApprenant.addAll(app.getContacts());
 		}
-		//selectedApprenant.add(selApp.toStringComplet());
 		spot = generalJList.getSelectedIndex();
 		
 		detailedList.setListData(selectedApprenant.toArray());
